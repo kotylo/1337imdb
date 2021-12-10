73e08fe14fc3a01cc6de5ba873dcd3710333d73a
kotylo <kotylo@gmail.com>
2021-12-10 23:42:04 +0100

    Move fetch to the background serivce worker (in progress)

--- src/background.js
+++ src/background.js
@@ -6,3 +6,62 @@ chrome.runtime.onInstalled.addListener(() => {
     });
     console.log("done");
 });
+
+chrome.runtime.onMessage.addListener( (request, sender, sendResponse) => {
+    let movie = request.movie;
+
+    console.log(sender.tab ?
+        "from a content script:" + sender.tab.url :
+        "from the extension");
+
+    fetch(`https://imdb.com/find?q=${movie.name}&s=tt&ttype=ft&ref_=fn_ft&count=3`)
+    .then(res => { res.text() })
+    .then(html => {
+        let parser = new DOMParser();
+        let doc = parser.parseFromString(html, "text/html");
+        let [link] = doc.getElementsByClassName("findResult");
+        if (link == null) {
+            console.error("link is null");
+            return;
+        }
+        let [a] = link.getElementsByTagName("a");
+        if (a == null) {
+            console.error("a is null");
+            return;
+        }
+        let href = a.getAttribute("href");
+        if (href == null) {
+            console.error("href is null");
+            return;
+        }
+        let [id] = href.split("/");
+        if (id == null) {
+            console.error("id is null");
+            return;
+        }
+        movie.id = id;
+        movie.href = href;
+        return movie;
+    })
+    .then(movie => {
+        if (movie == null) {
+            return;
+        }
+        return fetch(`https://imdb.com/${movie.href}`)
+        .then(res => { res.text() })
+        .then(html => {
+            let parser = new DOMParser();
+            let doc = parser.parseFromString(html, "text/html");
+            let [rating] = doc.getElementsByClassName("ratingValue");
+            if (rating == null) {
+                console.error("rating is null");
+                return;
+            }
+            movie.rating = rating.textContent;
+            return movie;
+        });
+    })
+    .then(movie => sendResponse({result: movie}));
+
+    return true;
+});
--- src/content-script.js
+++ src/content-script.js
@@ -1,4 +1,6 @@
 // get first element with class "featured-list"
+let moviesToCall = [];
+
 let [featuredList] = document.getElementsByClassName("featured-list");
 let trElements = featuredList.getElementsByTagName("tr");
 for (let i = 0; i < trElements.length; i++) {
@@ -10,17 +12,86 @@ for (let i = 0; i < trElements.length; i++) {
 
     //td.style.backgroundColor = "yellow";
     var moveieName = td.textContent;
+
     var [icon] = td.getElementsByClassName("icon");
     if (icon == null){
-        console.error("icon node is missing");
+        //console.error("icon node is missing");
         continue;
     }
 
-    let imdb = document.createElement("div");
-    imdb.innerHTML = `<a href="https://www.imdb.com/find?q=${moveieName}" target="_blank">IMDB</a>`;
-    insertAfter(imdb, icon);
+    moviesToCall.push({
+        rawName: moveieName,
+        name: getMovieName(moveieName),
+        iconElement: icon,
+    });
+
+    // temp testing 1 item
+    break;
 }
 document.body.style.backgroundColor = 'orange';
+
+Promise.all(moviesToCall.map(m => getMovieInfo(m)))
+.then(m => {
+    console.log(`${m.name} | rating: ${m.rating}`);
+    showRating(m);
+});
+
+const promiseTimeout = time => () => new Promise(resolve => setTimeout(resolve, time));
+
+function showRating(movie) {
+    let imdb = document.createElement("div");
+    imdb.textContent = `${movie.rating}`;
+
+    imdb.style.backgroundColor = 'yellow';
+    imdb.style.border = '1px solid black';
+    imdb.style.padding = '5px';
+    imdb.style.margin = '5px';
+    imdb.style.borderRadius = '5px';
+    imdb.style.fontSize = '20px';
+    imdb.style.color = 'black';
+    imdb.style.textAlign = 'center';
+    imdb.style.verticalAlign = 'middle';
+    imdb.style.lineHeight = '20px';
+    imdb.style.fontWeight = 'bold';
+    imdb.style.textShadow = '1px 1px 1px black';
+    imdb.style.boxShadow = '1px 1px 1px black';
+    imdb.style.backgroundImage = 'none';
+    imdb.style.backgroundColor = 'yellow';
+    imdb.style.backgroundRepeat = 'no-repeat';
+    imdb.style.backgroundPosition = 'center';
+    imdb.style.backgroundSize = 'contain';
+
+    insertAfter(imdb, m.iconElement);
+}
+
+function getMovieInfo(movie) {
+
+    return new Promise((resolve, reject) => {
+        chrome.runtime.sendMessage({movie: movie}, response => {
+            console.log(response.result);
+            if(response.result) {
+                resolve();
+            } else {
+                reject("Something went wrong with receiving the fetch response from background.js");
+            }
+        });
+    });
+
+    // chrome.runtime.sendMessage({movie: movie}, response =>{
+    //     console.log(response.result);
+    // });
+}
+
+function getMovieName(rawName) {
+    // parse name from raw name and replace . with space
+    let movieName = rawName.replace(/\./g, " ");
+    // remove everything after year in the movieName
+    let year = movieName.match(/\s\d{4}\s/);
+    if (year != null) {
+        movieName = movieName.substring(0, year.index + year.length);
+    }
+    return movieName;
+}
 
 function insertAfter(newNode, referenceNode) {
     referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
