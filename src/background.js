f8aba6feeaefe39302631dc657f394fbf701919b
kotylo <kotylo@gmail.com>
2021-12-11 15:04:53 +0100

    Change resolving of the Promise from all to one by one to have a live view

--- src/background.js
+++ src/background.js
@@ -7,61 +7,20 @@ chrome.runtime.onInstalled.addListener(() => {
     console.log("done");
 });
 
-chrome.runtime.onMessage.addListener( (request, sender, sendResponse) => {
-    let movie = request.movie;
-
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     console.log(sender.tab ?
-        "from a content script:" + sender.tab.url :
+        `fetch '${request.action}' request for movie '${request.movie.name}' from a content script: ${sender.tab.url}` :
         "from the extension");
 
-    fetch(`https://imdb.com/find?q=${movie.name}&s=tt&ttype=ft&ref_=fn_ft&count=3`)
-    .then(res => { res.text() })
-    .then(html => {
-        let parser = new DOMParser();
-        let doc = parser.parseFromString(html, "text/html");
-        let [link] = doc.getElementsByClassName("findResult");
-        if (link == null) {
-            console.error("link is null");
-            return;
-        }
-        let [a] = link.getElementsByTagName("a");
-        if (a == null) {
-            console.error("a is null");
-            return;
-        }
-        let href = a.getAttribute("href");
-        if (href == null) {
-            console.error("href is null");
-            return;
-        }
-        let [id] = href.split("/");
-        if (id == null) {
-            console.error("id is null");
-            return;
-        }
-        movie.id = id;
-        movie.href = href;
-        return movie;
-    })
-    .then(movie => {
-        if (movie == null) {
-            return;
-        }
-        return fetch(`https://imdb.com/${movie.href}`)
-        .then(res => { res.text() })
-        .then(html => {
-            let parser = new DOMParser();
-            let doc = parser.parseFromString(html, "text/html");
-            let [rating] = doc.getElementsByClassName("ratingValue");
-            if (rating == null) {
-                console.error("rating is null");
-                return;
-            }
-            movie.rating = rating.textContent;
-            return movie;
-        });
-    })
-    .then(movie => sendResponse({result: movie}));
+    if (request.action === "findMovie") {
+        fetch(`https://www.imdb.com/find?q=${request.movie.name}&s=tt&ttype=ft&ref_=fn_ft&count=3`)
+            .then(res => res.text())
+            .then(html => sendResponse(html));
+    } else if (request.action === "getMovie") {
+        fetch(`https://www.imdb.com${request.movie.href}`)
+            .then(res => res.text())
+            .then(html => sendResponse(html));
+    }
 
     return true;
 });
--- src/content-script.js
+++ src/content-script.js
@@ -25,71 +25,178 @@ for (let i = 0; i < trElements.length; i++) {
         iconElement: icon,
     });
 
-    // temp testing 1 item
-    break;
+    // temp testing 3 items
+    if (i > 5){
+        break;
+    }
 }
-document.body.style.backgroundColor = 'orange';
 
-Promise.all(moviesToCall.map(m => getMovieInfo(m)))
-.then(m => {
-    console.log(`${m.name} | rating: ${m.rating}`);
-    showRating(m);
-});
+document.body.style.backgroundColor = "orange";
+
+// Start loading all the movies from the page
+moviesToCall.map(m => getMovieInfo(m));
+
+// ************
+// functions
+// ************
 
 const promiseTimeout = time => () => new Promise(resolve => setTimeout(resolve, time));
 
 function showRating(movie) {
+    let link = document.createElement("a");
+    link.href = `https://www.imdb.com${movie.href}`;
+    link.target = "_blank";
+    insertAfter(link, movie.iconElement);
+
     let imdb = document.createElement("div");
     imdb.textContent = `${movie.rating}`;
+    imdb.style.backgroundColor = "#F5C518";
+    imdb.style.border = "1px solid black";
+    imdb.style.padding = "5px";
+    imdb.style.marginRight = "5px";
+    imdb.style.borderRadius = "3px";
+    imdb.style.fontSize = "12px";
+    imdb.style.color = "black";
+    imdb.style.textAlign = "center";
+    imdb.style.verticalAlign = "middle";
+    imdb.style.lineHeight = "3px";
+    imdb.style.fontWeight = "bold";
+    imdb.style.float = "left";
+    link.appendChild(imdb);
 
-    imdb.style.backgroundColor = 'yellow';
-    imdb.style.border = '1px solid black';
-    imdb.style.padding = '5px';
-    imdb.style.margin = '5px';
-    imdb.style.borderRadius = '5px';
-    imdb.style.fontSize = '20px';
-    imdb.style.color = 'black';
-    imdb.style.textAlign = 'center';
-    imdb.style.verticalAlign = 'middle';
-    imdb.style.lineHeight = '20px';
-    imdb.style.fontWeight = 'bold';
-    imdb.style.textShadow = '1px 1px 1px black';
-    imdb.style.boxShadow = '1px 1px 1px black';
-    imdb.style.backgroundImage = 'none';
-    imdb.style.backgroundColor = 'yellow';
-    imdb.style.backgroundRepeat = 'no-repeat';
-    imdb.style.backgroundPosition = 'center';
-    imdb.style.backgroundSize = 'contain';
-
-    insertAfter(imdb, m.iconElement);
+    let votesContainer = document.createElement("div");
+    votesContainer.style.float = "right";
+    votesContainer.style.fontSize = "5px";
+    votesContainer.textContent = `${movie.ratingCount} v.`;
+    votesContainer.style.marginLeft = "5px";
+    imdb.appendChild(votesContainer);
 }
 
 function getMovieInfo(movie) {
-
     return new Promise((resolve, reject) => {
-        chrome.runtime.sendMessage({movie: movie}, response => {
-            console.log(response.result);
-            if(response.result) {
-                resolve();
+        chrome.runtime.sendMessage({action: "findMovie", movie: movie}, html => {
+            if (html) {
+                resolve(html);
             } else {
-                reject("Something went wrong with receiving the fetch response from background.js");
+                reject(`Something went wrong with receiving the 'findMovie' fetch response from background.js for movie '${movie.name}'`);
             }
         });
-    });
+    })
+    .then(html => {
+        let parser = new DOMParser();
+        let doc = parser.parseFromString(html, "text/html");
+        let [link] = doc.getElementsByClassName("findResult");
+        if (link == null) {
+            console.error(`link is null for movie '${movie.name}'`);
+            return;
+        }
+        let [a] = link.getElementsByTagName("a");
+        if (a == null) {
+            console.error("a is null");
+            return;
+        }
+        let href = a.getAttribute("href");
+        if (href == null) {
+            console.error("href is null");
+            return;
+        }
+        let [id] = href.split("/");
+        if (id == null) {
+            console.error("id is null");
+            return;
+        }
+        movie.id = id;
+        movie.href = href;
+        return movie;
+    })
+    .then(movie => {
+        if (movie == null) {
+            return;
+        }
 
-    // chrome.runtime.sendMessage({movie: movie}, response =>{
-    //     console.log(response.result);
-    // });
+        return new Promise((resolve, reject) => {
+            chrome.runtime.sendMessage({action: "getMovie", movie: movie}, html => {
+                if (html) {
+                    resolve(html);
+                } else {
+                    reject(`Something went wrong with receiving the 'getMovie' fetch response from background.js for movie '${movie.name}'`);
+                }
+            });
+        })
+        .then(html => {
+            // find the rating inside the script tag with type="application/ld+json"
+            let parser = new DOMParser();
+            let doc = parser.parseFromString(html, "text/html");
+
+            let scripts = doc.getElementsByTagName("script");
+            if (scripts.length == 0) {
+                console.error("there are no script tags");
+                return;
+            }
+
+            var script = getScriptWithType(scripts, "application/ld+json");
+            if (script == null) {
+                console.error("script is null");
+                return;
+            }
+
+            let [json] = script.textContent.split("\n");
+            if (json == null) {
+                console.error("json is null");
+                return;
+            }
+            var parsedJson = JSON.parse(json);
+            let rating = parsedJson.aggregateRating.ratingValue;
+            if (rating == null) {
+                console.error("rating is null");
+                return;
+            }
+
+            let ratingCount = parsedJson.aggregateRating.ratingCount;
+            if (ratingCount == null) {
+                console.error("ratingCount is null");
+                return;
+            }
+
+            movie.rating = rating;
+            movie.ratingCount = ratingCount;
+            return movie;
+        });
+    })
+    .then(movie => {
+        if (movie == null) {
+            return;
+        }
+        console.log(`${movie.name} | rating: ${movie.rating}`);
+        showRating(movie);
+    });
+}
+
+function getScriptWithType(scripts, type) {
+    for (let i = 0; i < scripts.length; i++) {
+        let script = scripts[i];
+        let scriptType = script.getAttribute("type");
+        if (scriptType == null) {
+            continue;
+        }
+        if (scriptType == type) {
+            return script;
+        }
+    }
+    return null;
 }
 
 function getMovieName(rawName) {
     // parse name from raw name and replace . with space
     let movieName = rawName.replace(/\./g, " ");
+    // remove left parenthesis and right parenthesis
+    movieName = movieName.replace(/[\(\)]/g, "");
     // remove everything after year in the movieName
     let year = movieName.match(/\s\d{4}\s/);
-    if (year != null) {
-        movieName = movieName.substring(0, year.index + year.length);
+    if (year.length > 0) {
+        movieName = movieName.substring(0, year.index + year[0].length);
     }
+    movieName = movieName.trim();
     return movieName;
 }
 
